941aa0ba520ed8a74c239cc186196c41417ac09b
Farit Sibgatulin <farit_sib@Farits-MacBook-Air.local>
2021-11-20 11:02:22 +0300

    Farit commit

---
+++ pages/blog.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import { ReactElement } from "react";
+import Layout from "../components/layouts/Layout";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div>
+      <div>blogpage</div>
+    </div>
+  );
+}
+
+Home.getLayout = function getLayout(page: ReactElement) {
+  return <Layout>{page}</Layout>;
+};

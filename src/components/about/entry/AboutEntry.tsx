9addc65b5c0831a1537584bbd7cbbc6bc211e0cb
Eaeajj <eccentricmadm@gmail.com>
2021-11-28 21:02:11 +0300

    Test images

--- src/components/about/entry/AboutEntry.tsx
+++ src/components/about/entry/AboutEntry.tsx
@@ -8,7 +8,8 @@ interface AboutEntryProps {}
 const AboutEntry: React.FC<AboutEntryProps> = ({}) => {
   return (
     <EntryWrapper
-      img="https://www.acq-intl.com/wp-content/uploads/2020/01/tech-cruve.jpg"
+      // img="https://www.acq-intl.com/wp-content/uploads/2020/01/tech-cruve.jpg"
+      img="../../../../public/assets/planet.webp"
       alt="blog entry image"
     >
       <EntryTitle text="Очень рады познакомиться. Мы SILCODE. А вы?" />

1aadaa1ca27e8ad953aab421c5fff8722c071766
Farit Sibgatulin <farit_sib@Farits-MacBook-Air.local>
2021-12-01 15:40:50 +0300

    Add course card

--- src/pages/courses.tsx
+++ src/pages/courses.tsx
@@ -1,11 +1,13 @@
 import React, { ReactElement } from "react";
 import FHLayout from "../components/common/layouts/HFLayout";
 import CoursesEntry from "../components/courses/entry/CoursesEntry";
+import CourseList from "../components/courses/courseList/CourseList";
 
 const Courses = () => {
   return (
     <main>
       <CoursesEntry />
+      <CourseList />
     </main>
   );
 };
